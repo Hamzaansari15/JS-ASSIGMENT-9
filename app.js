e846d1206d875ed107848ec0a921124d98f83785
Hamzaansari <hamzaansaree15@gmail.com>
2022-07-07 13:54:23 -0700

    HTML AND JS FILES ADDED

---
+++ app.js
@@ -0,0 +1,4 @@
+// var date = new Date();
+// document.write(date);
+var arr = ["January","February","March","April","May","June","July","August","September	","October","November","December"];
+var date = new Date
